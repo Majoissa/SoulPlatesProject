3779ea66cd22bc88422c9c56b0196018dbee2427
Samuel-Riedel <samuel.riedel@outlook.com>
2023-10-12 12:32:59 +0200

    footer-version-1

---
+++ react-app/src/components/footer.jsx
@@ -0,0 +1,83 @@
+import React from "react";
+import "./footer.component.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="sb__footer section__padding">
+        <div className="sb__footer-links">
+          <div className="sb__footer-links_div">
+            <h4>Donations</h4>
+            <a href="/employer">
+              <p>Social Help</p>
+            </a>
+            <a href="/Health Plan">
+              <p>Health Plan</p>
+            </a>
+            <a href="/Individual">
+              <p>Individual</p>
+            </a>
+          </div>
+          <div className="sb__footer-links_div">
+            <h4>Volunteer</h4>
+            <a href="/resource">
+              <p>Center</p>
+            </a>
+            <a href="/resource">
+              <p>Testimonials</p>
+            </a>
+            <a href="/resource">
+              <p>Join</p>
+            </a>
+          </div>
+          <div className="sb__footer-links_div">
+            <h4>Partners</h4>
+            <a href="/employer">
+              <p>MigraCode</p>
+            </a>
+          </div>
+          <div className="sb__footer-links_div">
+            <h4>Company</h4>
+            <a href="/About">
+              <p>About</p>
+            </a>
+            <a href="/press">
+              <p>Press</p>
+            </a>
+            <a href="/career">
+              <p>Career</p>
+            </a>
+          </div>
+
+          <div className="sb__footer-links_div">
+            
+            <h4> Coming Soon</h4>
+            <div className="socialmedia">
+              <p><img src="https://i.postimg.cc/DmksBdCJ/facebook.png" alt="facebook"/></p>
+              <p><img src="https://i.postimg.cc/Wbgfj9cy/Instagram.png" alt="Instagram"/></p>
+              <p><img src="https://i.postimg.cc/c1V5Vp2Y/linkdn.png" alt="linkdn"/></p>
+              <p><img src="https://i.postimg.cc/PfLRVX3T/twitter.png" alt="twitter"/></p>
+
+            </div> 
+            
+          </div>
+        </div>
+      </div>
+      <hr></hr>
+      <div className="sb__footer-below">
+        <div className="sb__footer-copyright">
+          <p>Copyright @{new Date().getFullYear()} Soul Plates. All rights reserved.</p>
+        </div>
+        <div className="sb__footer-below-links">
+          <a href="/terms"><div><p>Terms & Conditions</p></div></a>
+          <a href="/privacy"><div><p>Privacy</p></div></a>
+          <a href="/security"><div><p>Security</p></div></a>
+          <a href="/cookies"><div><p>Cookies</p></div></a>
+
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
