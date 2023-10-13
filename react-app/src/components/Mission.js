2ef44220f59caf35ba3b920a48249375b5db6d31
María José Issa <95626214+Majoissa@users.noreply.github.com>
2023-10-13 16:06:31 +0200

    cambios

--- react-app/src/components/Mission.js
+++ react-app/src/components/Mission.js
@@ -1,36 +1,43 @@
 import React from "react";
-import './Mission-Goals.css';
+import "./Mission-Goals.css";
 
 function Mission() {
+  return (
+    <div className="Mission-and-Goals" style={{ backgroundColor: " #F7F5D2" }}>
+      <div className="Mission">
+        <h2>Our Mission</h2>
 
-    return (
-      
-        <div className="Mission-and-Goals" style={{backgroundColor:' #38387A'}} >
+        <p
+          style={{
+            marginTop: "30px",
+            fontSize: "1rem",
+            fontWeight: "bold",
+            textAlign: "justify",
+            color: "#323345",
+          }}
+        >
+          At Soul Plates, our mission is deeply rooted in the belief that every
+          individual, regardless of their background, deserves the opportunity
+          to thrive and build a better future. We are dedicated to providing
+          shelter, food, and education to all people who are in vulnerable
+          situations, offering them a safe heaven where they can rebuild their
+          lives. Our commitment is not just to meet immediate needs but to
+          empower these newcomers with the tools, knowledge, and support they
+          require to integrate into their new communities successfully. By
+          providing a holistic approach to support, we aim to foster a sense of
+          belonging and self-sufficiency, enabling them to embrace their dreams
+          and contribute to the vibrant tapestry of their new home.
+        </p>
+      </div>
 
-            <div className="Mission">
-
-              <h2>Our Mission</h2>
-
-              <p style={{marginTop: '30px', fontSize: '0.8rem', fontWeight:'bold'}}>At Soul Plates, our mission is deeply rooted in the belief that every individual, regardless of their background, deserves the opportunity to thrive and build a better future. We are dedicated to providing shelter, food, and education to all people who are in vulnerable situations, offering them a safe heaven where they can rebuild their lives. Our commitment is not just to meet immediate needs but to empower these newcomers with the tools, knowledge, and support they require to integrate into their new communities successfully. By providing a holistic approach to support, we aim to foster a sense of belonging and self-sufficiency, enabling them to embrace their dreams and contribute to the vibrant tapestry of their new home.</p>
-
-            </div>
-
-            <div className="mission-image">
-               
-
-             <img src="https://media.istockphoto.com/id/1372606861/photo/volunteers-hand-giving-donations-to-a-person-at-a-community-center.webp?b=1&s=612x612&w=0&k=20&c=sR0us7a7a71LVFwNYgHQvhiK0LFpCUcWnCecC92DL5o="
-              />
-
-               
-
-            </div>
-         
-
-        </div>
-
-
-    )
-
+      <div className="mission-image">
+        <img
+          src="https://media.istockphoto.com/id/1372606861/photo/volunteers-hand-giving-donations-to-a-person-at-a-community-center.webp?b=1&s=612x612&w=0&k=20&c=sR0us7a7a71LVFwNYgHQvhiK0LFpCUcWnCecC92DL5o="
+          alt="description component img"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Mission;
