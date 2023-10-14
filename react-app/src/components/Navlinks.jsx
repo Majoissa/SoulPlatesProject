976d3a6c2d4c3782cc7dbe2e31fced17547c165f
María José Issa <95626214+Majoissa@users.noreply.github.com>
2023-10-14 13:18:21 +0200

    Changed the files extensions to .jsx, added the routes, and an explanation to the README file

---
+++ react-app/src/components/Navlinks.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function NavLinks() {
+  return (
+    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+      <li className="nav-item">
+        <Link to="/" className="nav-link">
+          HOME
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link to="/" className="nav-link">
+          ABOUT US
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link to="/beneficiaries" className="nav-link">
+          BENEFICIARIES
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link to="/" className="nav-link">
+          VOLUNTEERS
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link to="/" className="nav-link">
+          DONORS
+        </Link>
+      </li>
+    </ul>
+  );
+}
+
+export default NavLinks;
