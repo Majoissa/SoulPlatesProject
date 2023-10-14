3d66c8720bf5dca526dae042f9961db504df51d3
RuchiJangra <ruchijangra2022@gmail.com>
2023-10-15 00:54:53 +0200

    social media done

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,4 +1,3 @@
-
 import React from "react";
 import Navbar from "./components/Navbar";
 import Info from "./components/Info";
@@ -7,21 +6,24 @@ import Mission from "./components/Mission";
 import HomeCards from "./components/Home-cards";
 import MainForm from "./components/Main-Form";
 import Goals from "./components/Goals";
-import NgoStory from "./components/NgoStory";
-
+//import NgoStory from "./components/NgoStory";
+import AdminLoginButton from "./components/AdminLoginButton";
+import SocialMedia from "./components/SocialMedia";
 function App() {
   return (
     <div>
       <Navbar />
       <Info />
+      <SocialMedia/>
       <Mission />
       <Goals />
       <HomeCards />
-      <NgoStory/>
       <MainForm />
       <Footer />
+      <AdminLoginButton/>
     </div>
   );
 }
-
 export default App;
+
+
---
+++ react-app/src/components/SocialMedia.jsx
@@ -0,0 +1,15 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faPhoneVolume } from "@fortawesome/free-solid-svg-icons";
+import { faMapLocationDot } from "@fortawesome/free-solid-svg-icons";
+import "./SocialMedia.css";
+function SocialMedia(){
+    return(
+        <div className="socialMedias">
+            <div><FontAwesomeIcon  className="iconSocial" icon={faMapLocationDot} /></div>
+            <div><FontAwesomeIcon className="iconSocial" icon={faEnvelope} /></div>
+            <div><FontAwesomeIcon className="iconSocial" icon={faPhoneVolume} /></div>
+        </div>
+    )
+}
+ export default SocialMedia;
