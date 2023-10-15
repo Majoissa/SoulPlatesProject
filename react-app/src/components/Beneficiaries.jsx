c018dbdc6699651f29f38985f90e8b702c37f1fc
María José Issa <95626214+Majoissa@users.noreply.github.com>
2023-10-15 22:28:32 +0200

    styles changes

--- react-app/src/components/Beneficiaries.jsx
+++ react-app/src/components/Beneficiaries.jsx
@@ -1,11 +1,18 @@
 import Navbar from "./Navbar";
 import Footer from "./footer";
+import BeneficiaryVideo from "./Beneficiary-video";
+import TextCards from "./Beneficiary-text-cards";
+import AdminLoginButton from "./AdminLoginButton";
+import SocialMedia from "./SocialMedia";
 function Beneficiaries() {
   return (
     <div>
       <Navbar />
-      <h1>This is the beneficiaries page</h1>;
+      <BeneficiaryVideo/>
+      <SocialMedia/>
+      <TextCards />
       <Footer />
+      <AdminLoginButton/>
     </div>
   );
 }
