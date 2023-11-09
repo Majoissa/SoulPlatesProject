2fc023f8a8947eb4b1defec432a1d4f0c8c5fae8
María José Issa <95626214+Majoissa@users.noreply.github.com>
2023-11-09 22:43:36 +0100

    private route

--- nodeServer/server.js
+++ nodeServer/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
-const {Pool} = require("pg");
+const { Pool } = require("pg");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
@@ -11,94 +11,49 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(
-    cors({
-        origin: "http://localhost:3000", //React is running in port 3000
-        credentials: true,
-    })
+  cors({
+    origin: "http://localhost:3000", //React is running in port 3000
+    credentials: true,
+  })
 );
 
 // Configuración de pg-pool
 const pool = new Pool({
-    host: "127.0.0.1",
-    user: "nurcan",
-    password: "Change_password",
-    database: "soul_plates",
-    max: 10, // número máximo de clientes en el pool
-    idleTimeoutMillis: 30000, // tiempo máximo de inactividad antes de cerrar el cliente
+  host: "127.0.0.1",
+  user: "postgres",
+  password: "150396+Majo",
+  database: "soul_plates",
+  max: 10, // número máximo de clientes en el pool
+  idleTimeoutMillis: 30000, // tiempo máximo de inactividad antes de cerrar el cliente
 });
 
 app.post("/register", async (req, res) => {
-    const {username, password, email} = req.body;
+  const { username, password, email } = req.body;
 
-    if (!email.includes("@") || !email.includes(".")) {
-        return res
-            .status(400)
-            .json({error: "Invalid email address."});
+  if (!email.includes("@") || !email.includes(".")) {
+    return res.status(400).json({ error: "Invalid email address." });
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  try {
+    const client = await pool.connect();
+    const result = await client.query(
+      "INSERT INTO admins (username, email, password) VALUES ($1, $2, $3) RETURNING id",
+      [username, email, hashedPassword]
+    );
+    client.release();
+
+    res.json({ success: true });
+  } catch (err) {
+    if (err.code === "23505") {
+      return res.status(400).json({
+        error: "El nombre de usuario o correo electrónico ya está en uso.",
+      });
     }
-
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
-
-    try {
-        const client = await pool.connect();
-        const result = await client.query(
-            "INSERT INTO admins (username, email, password) VALUES ($1, $2, $3) RETURNING id",
-            [username, email, hashedPassword]
-        );
-        client.release();
-
-        res.json({success: true});
-    } catch (err) {
-        if (err.code === "23505") {
-            return res.status(400).json({
-                error: "El nombre de usuario o correo electrónico ya está en uso.",
-            });
-        }
-        res.status(500).json({error: "Error al registrar el usuario."});
-    }
-});
-
-//Login
-app.post("/admin/login", async (req, res) => {
-    const {username, password} = req.body;
-
-    try {
-        const client = await pool.connect();
-        const result = await client.query(
-            "SELECT * FROM admins WHERE username = $1",
-            [username]
-        );
-        client.release();
-
-        if (result.rows.length === 0) {
-            return res.status(401).json({message: "User not found"});
-        }
-
-        const user = result.rows[0];
-        const hashedPassword = user.password;
-
-        bcrypt.compare(password, hashedPassword, (err, result) => {
-            if (err) {
-                return res.status(500).json({message: "Password comparison error"});
-            }
-
-            if (result) {
-                const token = jwt.sign({userId: user.id}, process.env.SECRET_KEY, {
-                    expiresIn: "1h",
-                });
-                res.cookie("token", token, {
-                    httpOnly: true,
-                    sameSite: "none",
-                    secure: true,
-                });
-                return res.status(200).json({message: "Login successful"});
-            } else {
-                return res.status(401).json({message: "Invalid password"});
-            }
-        });
-    } catch (err) {
-        res.status(500).json({message: "Error during login"});
-    }
+    res.status(500).json({ error: "Error al registrar el usuario." });
+  }
 });
 
 /// LOGOUT
@@ -166,36 +121,36 @@ app.post("/contact/beneficiaries", async (req, res) => {
 
 //get information from beneficiaries table
 app.get("/beneficiaries", (req, res) => {
-    pool.query("SELECT * FROM beneficiaries", (error, result) => {
-        if (error) {
-            console.error("Error querying the database:", error);
-            return res.status(500).json({error: "Error querying the database"});
-        }
-        res.json(result.rows);
-    });
+  pool.query("SELECT * FROM beneficiaries", (error, result) => {
+    if (error) {
+      console.error("Error querying the database:", error);
+      return res.status(500).json({ error: "Error querying the database" });
+    }
+    res.json(result.rows);
+  });
 });
 
-
-
 app.post("/volunteers", function (req, res) {
-    console.log("received request")
-    const { first_name, last_name, email, phone_number, message } = req.body;
-  console.log(req.body)
-    const query = "INSERT INTO volunteers (first_name, last_name, email, phone_number, message) VALUES ($1, $2, $3, $4, $5)";
-    const values = [first_name, last_name, email, phone_number, message];
-    console.log({values})
-    console.log(query)
-    pool.query({
-        text: query,
-        values: values
+  console.log("received request");
+  const { first_name, last_name, email, phone_number, message } = req.body;
+  console.log(req.body);
+  const query =
+    "INSERT INTO volunteers (first_name, last_name, email, phone_number, message) VALUES ($1, $2, $3, $4, $5)";
+  const values = [first_name, last_name, email, phone_number, message];
+  console.log({ values });
+  console.log(query);
+  pool
+    .query({
+      text: query,
+      values: values,
     })
-        .then(() => {
-            res.status(200).json({ success: true }); // Respond with JSON indicating success
-        })
-        .catch((e) => {
-            console.error(e);
-            res.status(500).json({ error: "Error querying the database" });
-        });
+    .then(() => {
+      res.status(200).json({ success: true }); // Respond with JSON indicating success
+    })
+    .catch((e) => {
+      console.error(e);
+      res.status(500).json({ error: "Error querying the database" });
+    });
 });
 
 // Endpoint for obtain all the 'volunteering' information
@@ -210,8 +165,7 @@ app.get("/volunteering", async (req, res) => {
   }
 });
 
-
 const PORT = 5550;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
