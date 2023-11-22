35722bf41fa847d2424076df04364ba1ac23f898
Samuel-Riedel <samuel.riedel@outlook.com>
2023-11-22 20:48:13 +0100

    fixed-logo-andfotsize

--- react-app/src/components/history/History.jsx
+++ react-app/src/components/history/History.jsx
@@ -5,7 +5,7 @@ function OurHistory() {
   return (
     <>
       <Logo
-        imagen="https://i.postimg.cc/h49L83XP/Soul-Plates-Logo-Cropped-3-Centered.png"
+        imagen="https://i.postimg.cc/VNFG1xZD/Soul-Plates-Logo-Cropped-Black-PNG.png"
         text=""
         descripImg="Soul Plates"
       />
@@ -28,12 +28,23 @@ function OurHistory() {
           }}
         >
           <div>
-            <p style={{fontFamily:'Montserrat'}}>
+            <h1
+              style={{ fontFamily: "Montserrat", fontSize: "3rem" }}
+              className="soulplates-title-sr"
+            >
+              Soul Plates
+            </h1>
+            <p style={{ fontFamily: "Montserrat" }}>
               <br />
-              SoulPlates started with a simple yet powerful idea: no individual should go hungry or feel abandoned. Our founders, Nurcan, Maria Jose, Samuel, Rucci and Amine, driven by personal experiences and a shared vision of a more caring world, began by preparing and distributing meals in Barcelona. What began as a modest effort soon resonated with the community, drawing volunteers and supporters from all walks of life.
+              SoulPlates started with a simple yet powerful idea: no individual
+              should go hungry or feel abandoned. Our founders, Nurcan, Maria
+              Jose, Samuel, Rucci and Amine, driven by personal experiences and
+              a shared vision of a more caring world, began by preparing and
+              distributing meals in Barcelona. What began as a modest effort
+              soon resonated with the community, drawing volunteers and
+              supporters from all walks of life.
             </p>
           </div>
-          
         </div>
       </div>
     </>
